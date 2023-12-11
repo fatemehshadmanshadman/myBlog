ad680c381582292bd34d3cc5f0f3157daff9ab07
fatemeh <fatemehshadmanshadman@gmail.com>
2023-12-11 22:03:44 +0330

    create3

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+app_name="articles"
 urlpatterns = [
-    path('',views.list)
+    path('',views.list,name="list"),
+    path('<slug>',views.detail,name="detail"),
 ]
+urlpatterns+=staticfiles_urlpatterns()
--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from . import models
+from django.shortcuts import HttpResponse
 
 def list(request):
     articles=models.Articles.objects.all()
     arg={'articles':articles}
     return render(request,'articles/list.html',arg)
+
+def detail(request,slug):
+    # return HttpResponse(slug)
+    arti=models.Articles.objects.get(slug=slug)
+    return render(request,'articles/detail.html',{'arti':arti})
